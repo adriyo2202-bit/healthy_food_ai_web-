e0d9e89f90ffaf46fc1c3a9c66c5f33f82c1ded4
adriyocreates_demo <adriyo2202@gmail.com>
2026-09-19 22:36:15 +0530

    Fix Vercel build script submodule conflict

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ ! -d "flutter" ]; then
+if [ ! -f "flutter/bin/flutter" ]; then
   echo "Cloning Flutter..."
+  rm -rf flutter
   git clone https://github.com/flutter/flutter.git -b stable
 fi
 echo "Enabling Flutter Web..."
